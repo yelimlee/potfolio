00ca3c2e26844a072c6e8b8fc6e49137075fd660
yelimlee <limroman@naver.com>
2021-07-30 03:23:21 +0900

    yerim

---
+++ js/default.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){
+  
+  var swiper3 = new Swiper(".slide", {
+    slidesPerView: 4,
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    }
+  });
+});
