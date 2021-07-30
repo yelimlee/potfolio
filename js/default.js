33343c00adefdc0f09c30a6c55e8a6650c8b9731
yelimlee <limroman@naver.com>
2021-07-30 18:15:32 +0900

    yerim

--- js/default.js
+++ js/default.js
@@ -6,6 +6,10 @@ document.addEventListener("DOMContentLoaded", function(){
     autoplay: {
       delay: 2000,
       disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     }
   });
 });
