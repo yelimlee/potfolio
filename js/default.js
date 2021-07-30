c1161e45581dbaf0b87da0178aa828f4fe5862ae
yelimlee <limroman@naver.com>
2021-07-30 16:21:21 +0900

    yerim

--- js/default.js
+++ js/default.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
   
   var swiper3 = new Swiper(".slide", {
-    slidesPerView: 4,
+    slidesPerView: 3,
     loop: true,
     autoplay: {
       delay: 2000,
